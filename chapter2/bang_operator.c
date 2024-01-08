d3aadcf23ee38eb7fee84e6d3c024feb20decdc3
Shamim <abir09bs@gmail.com>
2024-01-08 17:03:56 +0100

    check for NULL too

--- chapter2/bang_operator.c
+++ chapter2/bang_operator.c
@@ -5,6 +5,7 @@ int main(void) {
     printf("!1 = %i\n", !1);
     printf("!3 = %i\n", !3);
     printf("!-2 = %i\n", !-2);
+    printf("!NULL = %i (NULL = 0)\n", !NULL);
 
     if (!0) {
         printf("!0=1 is true\n");
