0aaafa483ced1ccf917e8c8bed901c83ad11a2bb
Shamim <abir09bs@gmail.com>
2024-01-12 00:18:32 +0100

    add forgotten to set NULL thing

--- chapter2/getline.c
+++ chapter2/getline.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// https://man7.org/linux/man-pages/man3/getline.3.html
+
 int main(void) {
-    char* lineptr;
-    size_t size;
+    char* lineptr = NULL;
+    size_t size = 0;
     int n = getline(&lineptr, &size, stdin);
     if (n < 0) {
         return 1;
